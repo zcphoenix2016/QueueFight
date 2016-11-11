00c8a9649a923fd4f471047c559e994afb3d4381
zcphoenix2016 <13564240769@139.com>
2016-11-11 10:43:27 +0800

    refactor test cases with TEST_P

--- TestCases/QueueFightTests.cpp
+++ TestCases/QueueFightTests.cpp
@@ -3,6 +3,7 @@
 #include "../Include/Person.hpp"
 #include <list>
 #include <vector>
+#include <utility>
 
 class QueueFightTestSuite : public ::testing::Test
 {
@@ -23,77 +24,35 @@ public:
     std::list<Person> list = {};
 };
 
-TEST_F(QueueFightTestSuite, KarateShouldWinHipis)
+class QueueFightTwoPersonsComparisonTestSuite : public QueueFightTestSuite,
+                                                public ::testing::WithParamInterface<std::pair<Person, Person>>
 {
-    Person p0(0, PERSON_TYPE::KARATE), p1(1, PERSON_TYPE::HIPIS);
+};
 
-    qf.push(p1);
-    qf.push(p0);
 
-    list = {p1, p0};
-
+TEST_P(QueueFightTwoPersonsComparisonTestSuite, checkTwoPersonsComparisonResult)
+{
+    const Person l_first = GetParam().first;
+    const Person l_second = GetParam().second;
+    
+    qf.push(l_second);
+    qf.push(l_first);
+    
+    list = {l_second, l_first};
+    
     ASSERT_EQ(qf.m_queue, list);
 }
 
-TEST_F(QueueFightTestSuite, BlondGirlShouldWinHipis)
-{
-    Person p0(0, PERSON_TYPE::BLONDGIRL), p1(1, PERSON_TYPE::HIPIS);
-
-    qf.push(p1);
-    qf.push(p0);
-
-    list = {p1, p0};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, ScienceGirlShouldWinHipis)
-{
-    Person p0(0, PERSON_TYPE::SCIENCEGIRL), p1(1, PERSON_TYPE::HIPIS);
-
-    qf.push(p1);
-    qf.push(p0);
-
-    list = {p1, p0};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, BararianShouldWinHipis)
-{
-    Person p0(0, PERSON_TYPE::BARBARIAN), p1(1, PERSON_TYPE::HIPIS);
-
-    qf.push(p1);
-    qf.push(p0);
-
-    list = {p1, p0};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, BarbarianShouldWinKarate)
-{
-    Person p0(0, PERSON_TYPE::KARATE), p1(1, PERSON_TYPE::BARBARIAN);
-
-    qf.push(p0);
-    qf.push(p1);
-
-    list = {p0, p1};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, ScienceGirlShouldWinBlondGirl)
-{
-    Person p0(0, PERSON_TYPE::BLONDGIRL), p1(1, PERSON_TYPE::SCIENCEGIRL);
-
-    qf.push(p0);
-    qf.push(p1);
-
-    list = {p0, p1};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
+INSTANTIATE_TEST_CASE_P(QueueFightTwoPersonsComparisonTests,
+                        QueueFightTwoPersonsComparisonTestSuite,
+                        ::testing::Values(std::make_pair<Person, Person>({0, PERSON_TYPE::HIPIS}, {1, PERSON_TYPE::HIPIS}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::KARATE}, {1, PERSON_TYPE::HIPIS}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::BLONDGIRL}, {1, PERSON_TYPE::HIPIS}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::SCIENCEGIRL}, {1, PERSON_TYPE::HIPIS}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::BARBARIAN}, {1, PERSON_TYPE::HIPIS}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::BARBARIAN}, {1, PERSON_TYPE::KARATE}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::SCIENCEGIRL}, {1, PERSON_TYPE::BLONDGIRL}),
+                                          std::make_pair<Person, Person>({0, PERSON_TYPE::SCIENCEGIRL}, {1, PERSON_TYPE::BARBARIAN})));
 
 TEST_F(QueueFightTestSuite,TwoOrMoreBlondGirlsShouldWinKarate)
 {
@@ -107,31 +66,6 @@ TEST_F(QueueFightTestSuite,TwoOrMoreBlondGirlsShouldWinKarate)
     qf.push(p0);
 
     list = {p3, p4, p0, p1, p2};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, ScienceGirlShouldWinBararian)
-{
-    Person p0(0, PERSON_TYPE::SCIENCEGIRL), p1(1, PERSON_TYPE::BARBARIAN);
-
-    qf.push(p1);
-    qf.push(p0);
-
-    list = {p1, p0};
-
-    ASSERT_EQ(qf.m_queue, list);
-}
-
-TEST_F(QueueFightTestSuite, NewHipisShouldWinExistingHipis)
-{
-    Person p0(0, PERSON_TYPE::HIPIS), p1(1, PERSON_TYPE::HIPIS), p2(2, PERSON_TYPE::HIPIS);
-
-    qf.push(p2);
-    qf.push(p1);
-    qf.push(p0);
-
-    list = {p2, p1, p0};
 
     ASSERT_EQ(qf.m_queue, list);
 }
